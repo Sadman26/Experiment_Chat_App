c2aa419405f876ed0ed6ffca3464370c117e29b9
Sadman26 <fareless.ranger@gmail.com>
2023-01-08 15:21:26 +0600

    Profile Image Pic Set

--- app/src/main/java/com/example/chatapp/SetupProfileActivity.java
+++ app/src/main/java/com/example/chatapp/SetupProfileActivity.java
@@ -1,22 +1,30 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.chatapp.databinding.ActivitySetupProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class SetupProfileActivity extends AppCompatActivity {
     ActivitySetupProfileBinding binding;
     FirebaseAuth auth;
     FirebaseDatabase database;
     FirebaseStorage storage;
+    Uri selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,45 @@ public class SetupProfileActivity extends AppCompatActivity {
                 startActivityForResult(intent,45);
             }
         });
+        binding.setupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=binding.nameBox.getText().toString();
+                if(name.isEmpty()){
+                    binding.nameBox.setError("Please Type Name ! 🙄");
+                    return ;
+                }
+                if(selectedImage !=null){
+                    StorageReference reference=storage.getReference().child("Profiles").child(auth.getUid());
+                    reference.putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                           if(task.isSuccessful()){
+                               reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                   @Override
+                                   public void onSuccess(Uri uri) {
+                                      String imageUrl=uri.toString();
+                                      String uid=auth.getUid();
+                                      String phone=auth.getCurrentUser().getPhoneNumber();
+                                      String name=binding.nameBox.getText().toString();
+                                      User user=new User(uid,name,phone,imageUrl);
+                                      database.getReference().child("Users").child(uid)
+                                              .setValue(user)
+                                              .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                  @Override
+                                                  public void onSuccess(Void aVoid) {
+                                                      Intent intent=new Intent(SetupProfileActivity.this,MainActivity.class);
+                                                      startActivity(intent);
+                                                  }
+                                              });
+                                   }
+                               });
+                           }
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
@@ -43,6 +90,7 @@ public class SetupProfileActivity extends AppCompatActivity {
         if(data  != null){
             if(data.getData()!=null){
                 binding.profileimage.setImageURI(data.getData());
+                selectedImage = data.getData();
             }
         }
     }
